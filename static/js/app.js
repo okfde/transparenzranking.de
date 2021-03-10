ad4f8b0061e731c1f2585c7d00ff15f17f08446d
Gregor Weber <gregorwbr@gmail.com>
2021-03-10 15:49:28 +0100

    simplify data import and calc

--- static/js/app.js
+++ static/js/app.js
@@ -1,255 +1,161 @@
 var app = angular.module('App', []);
 
-app.factory('ranking', function($http) {
-    var overview = null;
-    var states = {};
+app.factory('ranking', function ($http) {
     var data = null;
     var categories = null;
-    var keys = null;
-    var info = null;
-
-    function load(cb) {
-        if (!data) {
-            Tabletop.init({
-                key: 'https://docs.google.com/spreadsheets/d/10QpOTuLp01kl2Dev8ueblqCUFK8GAJD0UJXV6rBhjeo/pubhtml',
-                parseNumbers: true,
-                callback: function (tabletop_data, tabletop) {
-                    data = tabletop_data;
-                    cb(null, tabletop_data);
-                }
-            })
-        } else {
-            cb(null, data)
-        }
-    }
 
     return {
-        getOverview: function (cb) {
-            if (!overview) {
-                $http.get('/static/js/data/overview.json').then(function(response) {
-                    overview = response.data;
-                    cb(null, overview);
+        getData: function (cb) {
+            if (!data) {
+                $http.get('/static/js/data.json').then(function (response) {
+                    data = response.data;
+                    cb(null, data);
                 });
             } else {
-                cb(null, overview);
+                cb(null, data);
             }
         },
-        getState: function(state, cb) {
-            if (!states[state]) {
-                $http.get('/static/js/data/states/' + state + '.json').then(function(response) {
-                    states[state] = response.data;
-                    cb(null, response.data)
-                });
-            } else {
-                cb(null, states[state])
-            }
-        },
-        getOverviewForState: function(state, cb) {
-            this.getState(state, function(err, data) {
-                categories = Object.keys(data).reduce(function(prev, key) {
-
-                    prev[key] = data[key].reduce(function(prev, elem) {
-                        return {points: prev.points + elem.erreichte_punkte, max: prev.max + elem.maximalpunkte}
-                    }, {points: 0, max: 0});
-                    return prev;
-                }, {});
-                categories['Gesamt'] = Object.keys(categories).reduce(function(prev, elem) {
-                    const { points, max } = categories[elem];
+        getOverviewForState: function (state, cb) {
+            this.getData(function (err, data) {
+                categories = Object.keys(data.categories).reduce((prev, category) => ({
+                    ...prev,
+                    [category]: data.states[state]
+                        .filter(item => item.kategorie == category)
+                        .reduce(
+                            (prev, item) => ({
+                                points: prev.points + item.erreichte_punkte,
+                                max: prev.max + item.maximalpunkte
+                            }),
+                            {points: 0, max: 0}
+                        )
+                }), {});
+                categories['Gesamt'] = Object.keys(categories).reduce(function (prev, elem) {
+                    const {points, max} = categories[elem];
                     return {
                         points: prev.points + points / max / Object.keys(categories).length,
                         max: 100
                     };
-                }, {points:0, max: 0});
+                }, {points: 0, max: 0});
                 cb(null, categories);
             })
         },
-        getKeysForStates: function(cb) {
-            if (!keys) {
-                $http.get('/static/js/data/keys.json').then(function(response) {
-                    keys = response.data;
-                    cb(null, keys);
-                });
-            } else {
-                cb(null, keys);
-            }
-
-            // load(function(err, data) {
-            //     cb(null, Object.keys(data));
-            // })
-        },
-        getCategoryNames: function(cb) {
-            if (!categories) {
-                $http.get('/static/js/data/categoryNames.json').then(function(response) {
-                    categories = response.data;
-                    cb(null, categories);
-                });
-            } else {
-                cb(null, categories);
-            }
-        },
-        getCategoryInfo: function(cb) {
-            if (!info) {
-                $http.get('/static/js/data/categoryInfo.json').then(function(response) {
-                    info = response.data;
-                    cb(null, info);
-                });
-            } else {
-                cb(null, info);
-            }
-        }
     }
 });
 
+const getMaxFor = (category, items) => (
+    items.filter(item => item.kategorie == category).reduce((sum, item) => sum + item.maximalpunkte, 0)
+);
+
 app.controller('BarchartCtrl', function ($scope, ranking) {
 
-    $scope.colors = ['#ffe500','#f7a600','#596b01','#9a0052', '#009ee3', '#004079', '#3695D8'];
+    $scope.colors = ['#ffe500', '#f7a600', '#596b01', '#9a0052', '#009ee3', '#004079', '#3695D8'];
 
     //$scope.activeColor = $scope.colors['Gesamt'];
     $scope.chart_data = [];
     $scope.categories = [];
     $scope.bardata = [];
     $scope.barcat = [];
-    $scope.barcolors = ['#ffe500','#ffe500','#ffe500','#ffe500','#ffe500','#ffe500','#ffe500','#ffe500','#ffcb64','#ffcb64'];
+    $scope.barcolors = ['#ffe500', '#ffe500', '#ffe500', '#ffe500', '#ffe500', '#ffe500', '#ffe500', '#ffe500', '#ffcb64', '#ffcb64'];
     $scope.overview_data = [];
     $scope.activeColor = "#f00";
     $scope.show_info = false;
-    $scope.cat_info = [];
+    $scope.cat_info = {};
     $scope.active_cat = '';
     $scope.active_info = '';
 
-    $scope.loadOverview = function() {
+    $scope.loadOverview = function () {
         $scope.show_info = false;
-        ranking.getOverview(function(err, data) {
+        ranking.getData(function (err, data) {
             if (err) {
                 console.error(err);
-            } else {
-                $scope.overview_data = data.entries;
-                $scope.drafts = data.drafts;
-                // $scope.activeColor = "#6dffd4";
-                var barcolors = [];
-                ranking.getKeysForStates(function(err, keys) {
-                    keys = keys.sort();
-                    $scope.bar = keys.map(function(elem, index) {
-                        var sum = Math.round(data.entries.reduce(function(prev, cat) {
-                            return prev + cat.entries[elem] / cat.max / data.entries.length;
-                        }, 0) * 100);
-                        barcolors.push('#3695D8');
-                        return {name: elem, sum: sum}
-                    });
-
-                    $scope.bar = _.sortBy($scope.bar, [({name}) => name != 'Bund', 'sum']).reverse();
-                    $scope.bardata = _.map($scope.bar, 'sum');
-                    $scope.barcat = _.map($scope.bar, 'name');
-                    $scope.barcat.map(function(elem, index) {
-                        if (data.drafts.indexOf(elem) > -1) {
-                            barcolors[index] = '#ff0000';
-                        }
-                    });
-                    $scope.barcaptions = $scope.barcat.map(function(elem) {
-                        return elem;
-                    });
-                    // barcolors[0] = '#ff0000';
-                    $scope.activeColor = barcolors;
-                });
-                $scope.loading_finished = true;
+                return;
             }
+            $scope.drafts = data.drafts;
+            // $scope.activeColor = "#6dffd4";
+            var barcolors = [];
+            $scope.bar = Object.keys(data.states).sort().map(function (name) {
+                var sum = Math.round(data.states[name].reduce(
+                    (prev, item, i, items) => prev + item.erreichte_punkte / getMaxFor(item.kategorie, items) / Object.keys(data.categories).length,
+                    0
+                ) * 100);
+                barcolors.push('#3695D8');
+                return {name, sum}
+            });
+
+            $scope.bar = _.sortBy($scope.bar, [({name}) => name != 'Bund', 'sum']).reverse();
+            $scope.bardata = _.map($scope.bar, 'sum');
+            $scope.barcat = _.map($scope.bar, 'name');
+            $scope.barcaptions = $scope.barcat;
+            // barcolors[0] = '#ff0000';
+            $scope.activeColor = barcolors;
+            $scope.loading_finished = true;
         });
     };
 
-    ranking.getCategoryInfo(function(err, data) {
-        $scope.cat_info = data;
-    })
-
-    $scope.catClick = function(category, color) {
+    $scope.catClick = function (category, color) {
         $scope.show_info = true;
         $scope.active_cat = category;
-        $scope.active_info = _.find($scope.cat_info, ['name', category])['info'];
-        var cat_data = _.find($scope.overview_data, function(elem) { return elem.name === category });
-        var barcolors = [];
-        var barentries = _.chain(cat_data.entries)
-            .reduce(function(result, value, key) {
-                result.push({ name:key, value:Math.round(value * 100 / cat_data.max) });
-                return result;
-            }, [])
-            .sortBy('name')
-            .value();
-        $scope.bar = _.map($scope.barcat, function(order, index) {
-            barcolors.push(color);
-            return _.find(barentries, ['name', order]);
-        });
-
-        $scope.bardata = _.map($scope.bar, 'value');
-        $scope.barcat = _.map($scope.bar, 'name');
-        $scope.barcat.map(function(elem, index) {
-            if ($scope.drafts.indexOf(elem) > -1) {
-                barcolors[index] = '#ff0000';
-            }
-        });
-        $scope.barcaptions = $scope.barcat.map(function(elem, index) {
-            if ($scope.drafts.indexOf(elem) > -1) {
-                return elem + ' (Entwurf)'
-            } else {
-                return elem;
-            }
-        });
-        $scope.activeColor = barcolors;
+        ranking.getData((err, data) => {
+            $scope.active_info = data.categories[category];
+            const barentries = Object.entries(data.states).map(([name, items]) => ({
+                name,
+                value: Math.round(
+                    100 *
+                    items.filter(i => i.kategorie == category).reduce((sum, i) => sum + i.erreichte_punkte, 0)
+                    /
+                    getMaxFor(category, items)
+                )
+            }));
+            $scope.bar = _.map($scope.barcat, (order, index) => _.find(barentries, ['name', order]));
+            $scope.bardata = _.map($scope.bar, 'value');
+            $scope.barcat = _.map($scope.bar, 'name');
+            $scope.barcaptions = $scope.barcat;
+            $scope.activeColor = Object.keys(data.states).map(() => color);
+        })
     };
 
-    ranking.getCategoryNames(function(err, data) {
-        if (!err) {
-            $scope.categories = data;
-        } else {
-            console.error(data);
-        }
-    });
-
-    $scope.changeBarData = function(property) {
-        $scope.chart_data = getSeriesForCategory(property);
-    };
+    ranking.getData((err, data) => {
+        $scope.categories = Object.keys(data.categories)
+    })
 
     $scope.loadOverview();
 });
 
-app.controller('MapCtrl', function($scope) {
+app.controller('MapCtrl', function ($scope) {
 
 });
 
-app.controller('StateCtrl', function($scope, ranking) {
+app.controller('StateCtrl', function ($scope, ranking) {
     $scope.overview_points = 0;
     $scope.overview_max = 100;
     $scope.overview_color = "#3695D8";
     $scope.informationsrechte_color = "#ffcb64";
     $scope.cat_colors = {
-        "Lobbyregister":"#ffe500",
-        "Legislativer Fußabdruck":"#f7a600",
-        "Karenzzeit":"#596b01",
-        "Verhaltensregeln":"#9a0052",
+        "Lobbyregister": "#ffe500",
+        "Legislativer Fußabdruck": "#f7a600",
+        "Karenzzeit": "#596b01",
+        "Verhaltensregeln": "#9a0052",
     };
     $scope.cat_names = {
-        "Lobbyregister":"Lobbyregister",
-        "Legislativer Fußabdruck":"Legislativer Fußabdruck",
-        "Karenzzeit":"Karenzzeit",
-        "Verhaltensregeln":"Verhaltensregeln",
+        "Lobbyregister": "Lobbyregister",
+        "Legislativer Fußabdruck": "Legislativer Fußabdruck",
+        "Karenzzeit": "Karenzzeit",
+        "Verhaltensregeln": "Verhaltensregeln",
     };
     $scope.data_cat = [];
     $scope._ = _;
 
-    // Accordeon
-
-    $scope.init = function(state) {
+    $scope.init = function (state) {
         $scope.state = state;
         loadData(state);
     };
 
-    $scope.getNumber = function(num) {
+    $scope.getNumber = function (num) {
         return new Array(num);
     }
 
     function loadData(state) {
-        ranking.getState(state, function(err, data) {
-            $scope.stateData = data;
-        });
-        ranking.getOverviewForState(state, function(err, data) {
+        ranking.getOverviewForState(state, function (err, data) {
             $scope.overview_data = data;
             const points = Math.round(100 * data['Gesamt'].points);
             $scope.overview_points = points;
@@ -262,13 +168,10 @@ app.controller('StateCtrl', function($scope, ranking) {
                     $scope.data_cat.push(curr);
                 }
             }
-            $scope.informationsrechte_points = data['Informationsrechte'].points;
-            $scope.informationsrechte_max = data['Informationsrechte'].max;
-            $scope.informationsrechte_label = Math.floor($scope.informationsrechte_points * 100 / $scope.informationsrechte_max) + "%";
         });
     }
 
-    $scope.indicator_click = function($event) { // trigger
+    $scope.indicator_click = function ($event) { // trigger
         var elem = $event.currentTarget;
         $(elem).next("dd").slideToggle("fast"); // blendet beim Klick auf "dt" die nächste "dd" ein.
         $(elem).find("span").toggleClass('fa-caret-down');
