4c8cc42f4869be5df18865fd00f6cb59efa6da8b
krmax44 <hi@krmax44.de>
2021-03-24 17:12:08 +0100

    fix: only fetch stats in production

--- src/data/fdsStats.js
+++ src/data/fdsStats.js
@@ -6,6 +6,10 @@ async function totalCount(url) {
 }
 
 export async function getStateStats(state) {
+  if (process.env.NODE_ENV === 'development') {
+    return { all: 1001, successRate: 50 };
+  }
+
   const allUrl = `https://fragdenstaat.de/api/v1/request/?format=json&jurisdiction=${state.fdsId}&limit=1`;
   const successfulUrl = allUrl + '&resolution=successful';
 
@@ -13,5 +17,5 @@ export async function getStateStats(state) {
   const successful = await totalCount(successfulUrl);
 
   const successRate = Math.round((successful / all) * 100);
-  return { all, successful, successRate };
+  return { all, successRate };
 }
